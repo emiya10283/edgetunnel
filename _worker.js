8ec47ac780ed61c5cec04d393055e3660c1fab2b
张小凡 <h583910283@gmail.com>
2025-09-05 04:52:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'f316c164-645a-4951-8270-a9e9afa0c045';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -4568,3 +4568,4 @@ async function nginx() {
 	`
     return text;
 }
+
